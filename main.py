02dc03d36358da3a3d2e2dae305b47bfec2b7a7f
Bradley Gatewood <gatewoodbt@outlook.com>
2020-10-20 00:56:01 -0400

    add Tile class

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 import pygame
 
 
-def get_tile_surface(index):
-    tile_size = 100
-    tile_color1 = (255, 255, 255)
-    tile_color2 = (255, 0, 0)
+# the grid (puzzle) has 16 cells (spaces) and 15 tiles (pieces)
+
+
+class Tile:
+    size = 100
+    color1 = (255, 0, 0)
+    color2 = (255, 255, 255)
     font_name = 'consolas'
     font_size = 42
     font_color = (0, 0, 0)
 
-    tile = pygame.Surface((tile_size, tile_size))
-    if (index % 2):
-        pygame.draw.rect(tile, (tile_color1), tile.get_rect())
-    else:
-        pygame.draw.rect(tile, (tile_color2), tile.get_rect())
-    font = pygame.font.SysFont(font_name, font_size, bold=True)
-    text = font.render(str(index), True, font_color)  # what is True?!
-    rect = text.get_rect()
-    rect.center = tile.get_rect().center
-    tile.blit(text, rect)
-    return tile
-
-
-def get_tiles():
-    # could this be a list comprehension?
-    tiles = []
-    for i in range(1, 16):
-        tiles.append(get_tile_surface(i))
-    return tiles
+    def __init__(self, num):
+        self.surf = pygame.Surface((self.size, self.size))
+        self.surf.fill(self.color1 if num % 2 else self.color2)
+        font = pygame.font.SysFont(self.font_name, self.font_size, bold=True)
+        text = font.render(str(num), True, self.font_color)  # antialias = True
+        rect = text.get_rect()
+        rect.center = self.surf.get_rect().center
+        self.surf.blit(text, rect)
 
 
 def draw_grid(tiles, screen):
@@ -41,7 +33,7 @@ def draw_grid(tiles, screen):
     for i in range(len(tiles)):
         x = (i % n) * (tile_size + offset)
         y = (i // n) * (tile_size + offset)
-        grid.blit(tiles[i], (x, y))
+        grid.blit(tiles[i].surf, (x, y))
 
     rect = grid.get_rect()
     rect.center = screen.get_rect().center
@@ -55,7 +47,8 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(screen_size)
 
-    tiles = get_tiles()
+    # tiles = get_tiles()  # create the grid
+    tiles = [Tile(i) for i in range(1, 16)]
 
     run = True
     while run:
@@ -68,7 +61,7 @@ if __name__ == '__main__':
             elif event.type == pygame.QUIT:
                 run = False
         
-        draw_grid(tiles, screen)
+        draw_grid(tiles, screen)  # draw the grid
         pygame.display.flip()
 
     pygame.quit()
